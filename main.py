b749a94a4e28ea85b68957104c111fe709ac8928
B1lli <62195677+createrX12@users.noreply.github.com>
2023-03-11 16:14:03 +0800

    V3.0.0 优化数据结构与读写方法
    优化后的chatlog的json文件是一个包含多个字典的列表，每个字典形如：
    {
        "chat_seq": 1,
        "hash": "9ece95298ad14d5e",
        "created_time": "1678521676.391929",
        "altered_time": null,
        "message": {
            "role": "user",
            "content": "你好",
            "keyword": [],
            "summary": ""
        }
    }
    
    chat_seq：对话轮次，用来标记本次信息在上下文中的位置；
    hash：对话哈希值，由时间戳，角色和内容计算而成；
    created_time：本行对话的创建时间；
    created_time：本行对话的修改时间；
    message：消息字典，包含以下值；
    role：在chatGPT眼中发本消息的人的身份，可修改；
    content：消息内容；
    keyword：消息关键词（还没做）
    summary：消息梗概（还没做）

--- main.py
+++ main.py
@@ -15,8 +15,7 @@ import re
 
 # 赋值固定的api_key
 # 测试用
-openai.api_key = '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'
-
+openai.api_key = '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'
 
 # 创建对话行的类
 class chat_row(ft.UserControl) :
@@ -64,107 +63,60 @@ class chat_row(ft.UserControl) :
         self.content = self.content_textfield.value
         renew_now_chat(chat_json_path=chat_json_path,hash_val=self.hash,content=self.content)
 
+# 创建对话窗口的类（没做好，还在学怎么做）
+class chat_window(ft.UserControl):
+    def __init__(self):
+        super(chat_window, self).__init__()
+
+    def build(self):
+        self.chat_list = ft.ListView ( expand=True, spacing=10, auto_scroll=True, padding=20 )
+        self.page.add(self.chat_list)
+        self.chat_text = ft.TextField ( hint_text="想和chatGPT说些什么？",filled=True,expand=True,multiline=True )
 
 
+        return ft.Column(
+            controls=[
+                self.chat_list,
+                ft.Row(
+                    controls=[
+                        self.chat_text,
+                        ft.ElevatedButton ( "对话", on_click=add_msg )
+                    ]
+                ),
+            ],
+        )
 
+    def add_msg(self,e) :
+        self.chat_list.controls.append ( chat_row ( 'user', self.chat_text.value ) )
+        self.chat_text.value = ""
+        self.page.update ()
+        self.chat_list.controls.append ( chat_row ( 'assistant', self.chat ( chat_text.value ) ) )
+        self.page.update ()
 
-
-
-
-# 设置全局继承文本的列表
-global success_lst
-success_lst = []
+    def chat(self,msg = None) :
+        try:
+            message = get_combined_data(chat_json_path)
+            completion = openai.ChatCompletion.create (
+                model="gpt-3.5-turbo",
+                messages=message
+            )
+            gpt_msg = completion.choices[0].message['content']
+            d_msg = decode_chr ( gpt_msg )
+            return d_msg
+        except openai.error.RateLimitError as e:
+            print(e)
+            print( "频率过高，请过一分钟再试，或更换API-KEY" )
+            pass
+        except Exception as e:
+            print( f"出现如下报错：\n{e}，如果无法重新发起对话，请尝试重启程序，或者检查自己的网络环境能否连接到openai的服务器" )
+        finally:
+            print( d_msg )
 
 
 '''
 读写chatlog的相关函数
 '''
-# 储存现有的每一句聊天信息
-def save_now_chat(chat_json_path:str,role: str, content: str) -> str :
-    '''
-    将每一段聊天信息储存到一个 JSON 文件中。以哈希值作为唯一标识符
-    :param chat_json_path: 聊天文件的路径
-    :param role: 发言者
-    :param content: 发言内容
-    :return hash_val: 哈希值
-    '''
-    # 生成哈希值
-    now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    str_to_hash = now + role + content
-    hash_obj = hashlib.blake2b(str_to_hash.encode('utf-8'), digest_size=8)
-    hash_val = hash_obj.hexdigest()
-
-    # 读取之前的内容
-    try:
-        with open(chat_json_path, 'r') as f:
-            data = json.load(f)
-    except FileNotFoundError:
-        data = {}
-
-    # 更新数据并写入文件
-    data[hash_val] = [{"role": role}, {"content": content}]
-    with open(chat_json_path, 'w') as f:
-        json.dump(data, f)
-    return hash_val
-
-
-# 根据聊天信息的哈希值，更新现有历史聊天列表
-def renew_now_chat(chat_json_path:str, hash_val:str, role:str=None, content:str=None) -> None:
-    '''
-    根据聊天信息的哈希值，更新现有历史聊天列表
-    :param chat_json_path: 聊天文件的路径
-    :param hash_val: 聊天信息的哈希值
-    :param role: 更新后的发言者（可选）
-    :param content: 更新后的发言内容（可选）
-    :return: None
-    '''
-    # 读取chat_renew_data.json文件
-    with open(chat_json_path, 'r') as f:
-        data = json.load(f)
-    # 找出哈希值相同的键值对，并更新它们的role和content
-    if hash_val in data:
-        if role:data[hash_val][0]['role'] = role
-        if content:data[hash_val][1]['content'] = content
-
-        # 将更新后的数据写回文件
-        with open(chat_json_path, 'w') as f:
-            json.dump(data, f)
-
-# 根据聊天信息的哈希值，获取单个键值对内的role和content
-def get_one_role_and_content(chat_json_path:str, hash_value:str) -> (str,str):
-    '''
-    根据聊天信息的哈希值，获取单个键值对内的role和content
-    :param chat_json_path: 聊天文件的路径
-    :param hash_value: 聊天信息的哈希值
-    :return: (发言者, 发言内容)
-    '''
-    with open(chat_json_path) as f:
-        data = json.load(f)
-        if hash_value in data:
-            values = data[hash_value]
-            role = values[0]['role']
-            content = values[1]['content']
-            return role, content
-        else:
-            return None
-
-# 获取特定文件内的所有role和content
-def get_combined_data(chat_json_path:str) -> list[dict[str, str]]:
-    '''
-    获取特定文件内的所有role和content
-    :param chat_json_path: JSON 聊天文件的路径
-    :return: 包含所有发言者和发言内容的列表
-    '''
-    with open( chat_json_path ) as f:
-        data = json.load(f)
-        combined_data = []
-        for key, value in data.items():
-            role = value[0]['role']
-            content = value[1]['content']
-            combined_data.append({"role": role, "content": content})
-        return combined_data
-
-# 创建chat数据记录的方法
+# 创建chat数据记录的方法，将聊天数据结构基于 JSON 文件进行存储
 def create_chat_json(save_path='./chatlog'):
     # 如果保存目录不存在，则创建目录
     if not os.path.exists(save_path):
@@ -178,9 +130,130 @@ def create_chat_json(save_path='./chatlog'):
     chat_json_path = os.path.join(save_path, chat_json_name)
     if not os.path.exists(chat_json_path):
         with open(chat_json_path, 'w') as f:
-            json.dump({}, f)
-
+            json.dump([], f, default=str)
     return chat_json_path
+
+
+# 储存现有的每一句聊天信息
+def save_now_chat(chat_json_path: str, role: str, content: str) -> str:
+    '''
+    将每一段聊天信息储存到一个 JSON 文件中。以哈希值作为唯一标识符
+    :param chat_json_path: 聊天文件的路径
+    :param role: 发言者
+    :param content: 发言内容
+    :return hash_val: 哈希值
+    '''
+    now = datetime.now().timestamp()
+    str_to_hash = str(now) + role + content
+    hash_obj = hashlib.blake2b(str_to_hash.encode('utf-8'), digest_size=8)
+    hash_val = hash_obj.hexdigest()
+
+    # 读取之前的内容
+    try:
+        with open(chat_json_path, 'r') as f:
+            chats = json.load(f)
+    except FileNotFoundError:
+        chats = []
+
+    # 添加新的聊天信息
+    message = {
+        'role': role,
+        'content': content,
+        'keyword': [],
+        'summary': ''
+    }
+    chats.append({
+        'chat_seq': len(chats) + 1,
+        'hash': hash_val,
+        'created_time': now,
+        'altered_time': None,
+        'message': message
+    })
+
+    # # 更新聊天信息中的关键字和摘要
+    # for chat in chats:
+    #     keywords = []
+    #     for msg in chat['message']:
+    #         for word in re.findall(r'\w+', msg['content']):
+    #             if len(word) > 1 and word not in keywords:
+    #                 keywords.append(word)
+    #         msg['keyword'] = keywords
+    #         msg['summary'] = '，'.join(keywords[:3]) + '...' if keywords else ''
+
+    # 将新的聊天信息写入文件
+    with open(chat_json_path, 'w') as f:
+        json.dump(chats, f, default=str)
+
+    return hash_val
+
+
+# 根据聊天信息的哈希值，更新现有历史聊天列表
+def renew_now_chat(chat_json_path: str, hash_val: str, role: str = None, content: str = None) -> None:
+    '''
+    根据聊天信息的哈希值，更新现有历史聊天列表
+    :param chat_json_path: 聊天文件的路径
+    :param hash_val: 聊天信息的哈希值
+    :param role: 更新后的发言者（可选）
+    :param content: 更新后的发言内容（可选）
+    :return: None
+    '''
+    # 读取chat_renew_data.json文件
+    with open(chat_json_path, 'r') as f:
+        data = json.load(f)
+
+    # 找出哈希值相同的键值对，并更新它们的role和content
+    for chat_item in data:
+        if hash_val == chat_item['hash']:
+            if role:
+                chat_item['message']['role'] = role
+            if content:
+                chat_item['message']['content'] = content
+            chat_item["altered_time"] = datetime.now().timestamp()
+            break
+
+    # 将更新后的数据写回文件
+    with open(chat_json_path, 'w') as f:
+        json.dump(data, f, default=str)
+
+
+# 根据聊天信息的哈希值，获取单个键值对内的role和content
+def get_one_role_and_content(chat_json_path: str, hash_value: str) -> (str, str):
+    '''
+    根据聊天信息的哈希值，获取单个键值对内的role和content
+    :param chat_json_path: 聊天文件的路径
+    :param hash_value: 聊天信息的哈希值
+    :return: (发言者, 发言内容)
+    '''
+    with open(chat_json_path) as f:
+        data = json.load(f)
+
+        for chat_item in data:
+            for message in chat_item['message']:
+                if hash_value == hashlib.blake2b(
+                        str(message["created_time"] + message["role"] + message["content"]).encode('utf-8'),
+                        digest_size=8).hexdigest():
+                    return message["role"], message["content"]
+
+        return None
+
+
+# 读取特定文件内的所有role和content
+def get_combined_data(chat_json_path: str) -> list[dict[str, str]]:
+    '''
+    获取特定文件内的所有role和content
+    :param chat_json_path: JSON 聊天文件的路径
+    :return: 包含所有发言者和发言内容的列表
+    '''
+    with open(chat_json_path) as f:
+        data = json.load(f)
+        result = []
+        for chat_item in data:
+            result.append({
+                "role": chat_item['message']["role"],
+                "content": chat_item['message']["content"]
+            })
+        return result
+
 
 # 创建chat数据记录
 chat_json_path = create_chat_json ()
@@ -253,43 +326,16 @@ def ft_interface(page: ft.Page) :
     gpt_text = ft.ListView ( expand=True, spacing=10, auto_scroll=True, padding=20 )
     page.add ( gpt_text )
 
-
-
-    # # 构造聊天行
-    # def chat_row(character,value):
-    #     return ft.Row(
-    #         [
-    #             ft.Dropdown(
-    #                 value=character,
-    #                 width=150,
-    #                 options=[
-    #                     ft.dropdown.Option("system"),
-    #                     ft.dropdown.Option("user"),
-    #                     ft.dropdown.Option("assistant"),
-    #                 ],
-    #             ),
-    #             ft.TextField(
-    #                 value=value,
-    #                 filled=True,
-    #                 # bgcolor='#1c1c1c',
-    #                 expand=True,
-    #                 multiline=True
-    #             ),
-    #         ]
-    #     )
-
-    # 按下对话按钮
-
     # 调用
     def add_msg(e):
         gpt_text.controls.append ( chat_row('user',chat_text.value) )
         chat_text.value = ""
         page.update()
-        gpt_text.controls.append(chat_row('assistant',chat2(chat_text.value)))
+        gpt_text.controls.append(chat_row('assistant',chat(chat_text.value)))
         page.update()
 
     # 设置布局
-    chat_text = ft.TextField ( hint_text="想和chatGPT说些什么？",filled=True,expand=True )
+    chat_text = ft.TextField ( hint_text="想和chatGPT说些什么？",filled=True,expand=True,multiline=True )
     view = ft.Column(
         controls=[
             ft.Row(
@@ -301,51 +347,31 @@ def ft_interface(page: ft.Page) :
         ],
     )
 
-
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(view)
 
 
 
-    def chat2(msg = None) :
-        global success_lst
-        try:
-            # msg = [{"role" : "user", "content" : f"{msg}"}]
-            # message = [
-            #     {"role" : "system",
-            #      "content" : '''你是名为Billy的助手'''}
-            # ]
-            # if success_lst :
-            #     success_lst.extend(msg)
-            #     message.extend ( success_lst )
-            # else:
-            #     message.extend(msg)
-            message = get_combined_data(chat_json_path)
-            completion = openai.ChatCompletion.create (
-                model="gpt-3.5-turbo",
-                messages=message
-            )
-            gpt_msg = completion.choices[0].message['content']
-            d_msg = decode_chr ( gpt_msg )
-            # success_lst.append({"role" : "assistant", "content" : f"{d_msg}"})
-            return d_msg
+    def chat(msg = None) :
+        message = get_combined_data(chat_json_path)
+        completion = openai.ChatCompletion.create (
+            model="gpt-3.5-turbo",
+            messages=message
+        )
+        gpt_msg = completion.choices[0].message['content']
+        d_msg = decode_chr ( gpt_msg )
+        return d_msg
 
-        except openai.error.RateLimitError:
-            print( "频率过高，请过一分钟再试，或更换API-KEY" )
-            pass
-        except Exception as e:
-            print( f"出现如下报错：\n{e}，如果无法重新发起对话，请尝试重启程序，或者检查自己的网络环境能否连接到openai的服务器" )
-        finally:
-            print( success_lst )
 
 
     # 版本信息
-    ver_text = ft.Text('V2.0.0 有技术问题请咨询qq2508285107',size=10)
+    ver_text = ft.Text('BillyGPT V3.0.0  By B1lli',size=10)
     page.add(ver_text)
+
 
 if __name__ == '__main__':
     ft.app ( target=ft_interface, assets_dir='assets' )
-
+    # print ( time.time () )
 
 
 
