ed89ba5d033b7e809141af0972c7ab1ecbf5a72f
B1lli <62195677+createrX12@users.noreply.github.com>
2023-03-13 11:44:58 +0800

    添加导入历史聊天记录功能

--- main.py
+++ main.py
@@ -24,106 +24,70 @@ from flet import (
 openai.api_key = None
 
 
-# 创建对话行的类
-class chat_row(ft.UserControl) :
+'''
+每一行对话的类
+在调用的同时，会将数据存入聊天记录文件
+'''
+
+
+class chat_row(ft.UserControl):
     def __init__(self, role, content):
         super(chat_row, self).__init__()
         self.role = role
         self.content = content
-        self.hash = save_now_chat(chat_json_path=chat_json_path,role=self.role,content=self.content)
+        self.hash = save_now_chat(
+            chat_json_path=chat_json_path,
+            role=self.role,
+            content=self.content)
 
-
-    def build(self) :
-        self.role_dropdown =ft.Dropdown (
-                    value=self.role,
-                    width=150,
-                    options=[
-                        ft.dropdown.Option ( "system" ),
-                        ft.dropdown.Option ( "user" ),
-                        ft.dropdown.Option ( "assistant" ),
-                    ],
+    def build(self):
+        self.role_dropdown = ft.Dropdown(
+            value=self.role,
+            width=150,
+            options=[
+                ft.dropdown.Option("system"),
+                ft.dropdown.Option("user"),
+                ft.dropdown.Option("assistant"),
+            ],
             on_change=self.role_change
 
-                )
+        )
 
-        self.content_textfield = ft.TextField (
-                    value=self.content,
-                    filled=True,
-                    expand=True,
-                    multiline=True,
+        self.content_textfield = ft.TextField(
+            value=self.content,
+            filled=True,
+            expand=True,
+            multiline=True,
             on_change=self.content_change
-                )
+        )
 
-        return ft.Row (
+        return ft.Row(
             [
                 self.role_dropdown,
                 self.content_textfield,
             ]
         )
 
-    def role_change(self,e):
+    def role_change(self, e):
         self.role = self.role_dropdown.value
-        renew_now_chat(chat_json_path=chat_json_path,hash_val=self.hash,role=self.role)
+        renew_now_chat(
+            chat_json_path=chat_json_path,
+            hash_val=self.hash,
+            role=self.role)
 
-
-    def content_change(self,e):
+    def content_change(self, e):
         self.content = self.content_textfield.value
-        renew_now_chat(chat_json_path=chat_json_path,hash_val=self.hash,content=self.content)
-
-# （没做好，还在学怎么做）创建对话窗口的类
-class chat_window(ft.UserControl):
-    def __init__(self):
-        super(chat_window, self).__init__()
-
-    def build(self):
-        self.chat_list = ft.ListView ( expand=True, spacing=10, auto_scroll=True, padding=20 )
-        self.page.add(self.chat_list)
-        self.chat_text = ft.TextField ( hint_text="想和chatGPT说些什么？",filled=True,expand=True,multiline=True )
-
-
-        return ft.Column(
-            controls=[
-                self.chat_list,
-                ft.Row(
-                    controls=[
-                        self.chat_text,
-                        ft.ElevatedButton ( "对话", on_click=add_msg )
-                    ]
-                ),
-            ],
-        )
-
-    def add_msg(self,e) :
-        self.chat_list.controls.append ( chat_row ( 'user', self.chat_text.value ) )
-        self.chat_text.value = ""
-        self.page.update ()
-        self.chat_list.controls.append ( chat_row ( 'assistant', self.chat ( chat_text.value ) ) )
-        self.page.update ()
-
-    def chat(self,msg = None) :
-        try:
-            message = get_combined_data(chat_json_path)
-            completion = openai.ChatCompletion.create (
-                model="gpt-3.5-turbo",
-                messages=message
-            )
-            gpt_msg = completion.choices[0].message['content']
-            d_msg = decode_chr ( gpt_msg )
-            return d_msg
-        except openai.error.RateLimitError as e:
-            print(e)
-            print( "频率过高，请过一分钟再试，或更换API-KEY" )
-            pass
-        except Exception as e:
-            print( f"出现如下报错：\n{e}，如果无法重新发起对话，请尝试重启程序，或者检查自己的网络环境能否连接到openai的服务器" )
-        finally:
-            print( d_msg )
-
+        renew_now_chat(
+            chat_json_path=chat_json_path,
+            hash_val=self.hash,
+            content=self.content)
 
 
 '''
 读写chatlog的相关函数
 '''
+
+
 # 创建chat数据记录的方法，将聊天数据结构基于 JSON 文件进行存储
 def create_chat_json(save_path='./chatlog'):
     # 如果保存目录不存在，则创建目录
@@ -178,7 +142,6 @@ def save_now_chat(chat_json_path: str, role: str, content: str) -> str:
         'message': message
     })
 
-
     # 将新的聊天信息写入文件
     with open(chat_json_path, 'w') as f:
         json.dump(chats, f, default=str)
@@ -187,7 +150,8 @@ def save_now_chat(chat_json_path: str, role: str, content: str) -> str:
 
 
 # 根据聊天信息的哈希值，更新现有历史聊天列表
-def renew_now_chat(chat_json_path: str, hash_val: str, role: str = None, content: str = None) -> None:
+def renew_now_chat(chat_json_path: str, hash_val: str,
+                   role: str = None, content: str = None) -> None:
     '''
     根据聊天信息的哈希值，更新现有历史聊天列表
     :param chat_json_path: 聊天文件的路径
@@ -216,7 +180,8 @@ def renew_now_chat(chat_json_path: str, hash_val: str, role: str = None, content
 
 
 # 根据聊天信息的哈希值，获取单个键值对内的role和content
-def get_one_role_and_content(chat_json_path: str, hash_value: str) -> (str, str):
+def get_one_role_and_content(
+        chat_json_path: str, hash_value: str) -> (str, str):
     '''
     根据聊天信息的哈希值，获取单个键值对内的role和content
     :param chat_json_path: 聊天文件的路径
@@ -229,7 +194,9 @@ def get_one_role_and_content(chat_json_path: str, hash_value: str) -> (str, str)
         for chat_item in data:
             for message in chat_item['message']:
                 if hash_value == hashlib.blake2b(
-                        str(message["created_time"] + message["role"] + message["content"]).encode('utf-8'),
+                        str(message["created_time"] +
+                            message["role"] +
+                            message["content"]).encode('utf-8'),
                         digest_size=8).hexdigest():
                     return message["role"], message["content"]
 
@@ -255,25 +222,26 @@ def get_combined_data(chat_json_path: str) -> list[dict[str, str]]:
 
 
 # 创建chat数据记录
-chat_json_path = create_chat_json ()
-
+chat_json_path = create_chat_json()
 
 
 '''
 读写API-KEY的函数
 '''
+
+
 # 定义 write_APIKEY 函数
 def write_APIKEY(APIKEY=None):
-# 以追加模式打开或创建 APIKEY.txt 文件
+    # 以追加模式打开或创建 APIKEY.txt 文件
     with open("APIKEY.txt", "a") as f:
         # 写入字符串并换行
-        if APIKEY:f.write(APIKEY + "\n")
-
+        if APIKEY:
+            f.write(APIKEY + "\n")
 
 
 # 定义 read_APIKEY 函数
 def read_APIKEY():
-# 以读取模式打开 APIKEY.txt 文件
+    # 以读取模式打开 APIKEY.txt 文件
     with open("APIKEY.txt", "r") as f:
         # 读取所有行并存入列表
         lines = f.readlines()
@@ -282,26 +250,27 @@ def read_APIKEY():
             return lines[-1].strip()
 
 
-
 '''
 其他函数
 '''
+
+
 # 字符转码
-def decode_chr(s) :
-    pattern = re.compile ( r'(\\u[0-9a-fA-F]{4}|\n)' )
+def decode_chr(s):
+    pattern = re.compile(r'(\\u[0-9a-fA-F]{4}|\n)')
     result = ''
     pos = 0
-    while True :
-        match = pattern.search ( s, pos )
-        if match is None :
+    while True:
+        match = pattern.search(s, pos)
+        if match is None:
             break
-        result += s[pos :match.start ()]
-        if match.group () == '\n' :
+        result += s[pos:match.start()]
+        if match.group() == '\n':
             result += '\n'
-        else :
-            result += chr ( int ( match.group ()[2 :], 16 ) )
-        pos = match.end ()
-    result += s[pos :]
+        else:
+            result += chr(int(match.group()[2:], 16))
+        pos = match.end()
+    result += s[pos:]
     return result
 
 
@@ -310,52 +279,50 @@ def markdown_check(gpt_msg):
     pass
 
 
-
 '''
 程序主窗体
 '''
-def ft_interface(page: ft.Page) :
+
+
+def ft_interface(page: ft.Page):
     # 设置字体与主题
     page.title = 'BillyGPT'
-    page.fonts = {'A75方正像素12' : './assets/font.ttf'}
-    page.theme = ft.Theme ( font_family='A75方正像素12' )
+    page.fonts = {'A75方正像素12': './assets/font.ttf'}
+    page.theme = ft.Theme(font_family='A75方正像素12')
     page.dark_theme = page.theme
 
-
-
-
-    '''
-    （没写好）添加报错提示对话
-    '''
-    global error_msg
-    error_msg = ''
-    def open_alert(e):
-        alert_dlg.open = True
-        page.update ()
-        pass
-
-    alert_dlg = ft.AlertDialog(
-        title=ft.Text("错误"), on_dismiss=lambda e: print("Dialog dismissed!")
-    )
-
-
-
-
+    # 设置主页面聊天区域的滚动列表
+    gpt_text = ft.ListView(
+        expand=True,
+        spacing=10,
+        auto_scroll=True,
+        padding=20)
 
     '''
     添加选择上传文件、保存文件、打开文件夹按钮
     '''
-    # 上传
+    # 导入聊天记录
     def pick_files_result(e: FilePickerResultEvent):
-        selected_files.value = (
-            ", ".join(map(lambda f: f.name, e.files)) if e.files else "Cancelled!"
-        )
-        selected_files.update()
+        try:
+            gpt_text.controls.clear()
+            selected_file = (
+                ", ".join(map(lambda f: f.path, e.files)
+                          ) if e.files else "Cancelled!"
+            )
+            full_chatlog = get_combined_data(selected_file)
+            print(full_chatlog)
+            for chat_row_content in full_chatlog:
+                role = chat_row_content['role']
+                content = chat_row_content['content']
+                gpt_text.controls.append(chat_row(role, content))
+                page.update()
+        except Exception as e:
+            gpt_text.controls.append(
+                Text(f'出现如下报错\n{e}\n请检查导入的聊天记录是否正确，或联系开发者微信B1lli_official'))
 
     pick_files_dialog = FilePicker(on_result=pick_files_result)
-    selected_files = Text()
 
-    # 保存
+    # 导出聊天记录（还没做）
     def save_file_result(e: FilePickerResultEvent):
         save_file_path.value = e.path if e.path else "Cancelled!"
         save_file_path.update()
@@ -363,7 +330,7 @@ def ft_interface(page: ft.Page) :
     save_file_dialog = FilePicker(on_result=save_file_result)
     save_file_path = Text()
 
-    # 打开文件夹
+    # 打开文件夹（还没做）
     def get_directory_result(e: FilePickerResultEvent):
         directory_path.value = e.path if e.path else "Cancelled!"
         directory_path.update()
@@ -372,12 +339,8 @@ def ft_interface(page: ft.Page) :
     directory_path = Text()
 
     # 隐藏所有
-    page.overlay.extend([pick_files_dialog, save_file_dialog, get_directory_dialog])
-
-
-
-
-
+    page.overlay.extend(
+        [pick_files_dialog, save_file_dialog, get_directory_dialog])
 
     '''
     添加设置对话和按钮
@@ -385,7 +348,7 @@ def ft_interface(page: ft.Page) :
     def save_settings(e):
         settings_dlg.open = False
         write_APIKEY(apikey_field.value)
-        read_APIKEY ()
+        read_APIKEY()
         page.update()
 
     def cancel_settings(e):
@@ -398,7 +361,7 @@ def ft_interface(page: ft.Page) :
         content=apikey_field,
         actions=[
             ft.TextButton("保存", on_click=save_settings),
-            ft.TextButton ( "取消", on_click=cancel_settings )
+            ft.TextButton("取消", on_click=cancel_settings)
         ],
         actions_alignment=ft.MainAxisAlignment.END,
         shape=ft.RoundedRectangleBorder(radius=10)
@@ -410,17 +373,54 @@ def ft_interface(page: ft.Page) :
         page.update()
 
     settings_btn = ft.IconButton(
-                    icon=ft.icons.SETTINGS_OUTLINED,
-                    icon_color="#9ecaff",
+        icon=ft.icons.SETTINGS_OUTLINED,
+        icon_color="#9ecaff",
         bgcolor='#202429',
-                    icon_size=20,
-                    tooltip="Settings",
+        icon_size=20,
+        tooltip="Settings",
         on_click=open_dlg_modal,
-                )
+    )
+
+    '''
+    添加启动应用程序获取apikey窗口
+    '''
+    def save_settings_open(e):
+        open_setting_apikey_dlg.open = False
+        write_APIKEY(apikey_field_open.value)
+        openai.api_key = read_APIKEY()
+        page.update()
+
+    write_APIKEY()
+    openai.api_key = read_APIKEY()
+    if not openai.api_key:
+        apikey_field_open = ft.TextField(label="输入你的apikey")
+        open_setting_apikey_dlg = ft.AlertDialog(
+            open=True,
+            modal=True,
+            title=ft.Text("欢迎使用BillyGPT"),
+            content=ft.Column([apikey_field_open], tight=True),
+            actions=[
+                ft.ElevatedButton(
+                    text="开始使用",
+                    on_click=save_settings_open)],
+            actions_alignment="end",
+        )
+        page.dialog = open_setting_apikey_dlg
+        openai.api_key = read_APIKEY()
 
 
     '''
-    添加控件
+    添加聊天行
+    '''
+    def add_msg(e):
+        gpt_text.controls.append(chat_row('user', chat_text.value))
+        chat_text.value = ""
+        page.update()
+        gpt_text.controls.append(chat_row('assistant', chat(chat_text.value)))
+        page.update()
+
+    '''
+    设置布局 添加控件
     '''
     page.add(
         Row(
@@ -429,116 +429,75 @@ def ft_interface(page: ft.Page) :
                     "导入聊天日志",
                     icon=icons.UPLOAD_FILE,
                     on_click=lambda _: pick_files_dialog.pick_files(
-                        allow_multiple=True
+                        allowed_extensions=['json'],
+                        allow_multiple=False,
+                        dialog_title='选择聊天记录文件导入',
+                        initial_directory='./chatlog'
                     ),
                 ),
-                ElevatedButton (
+                ElevatedButton(
                     "保存聊天日志",
                     icon=icons.SAVE,
-                    on_click=lambda _ : save_file_dialog.save_file (),
+                    on_click=lambda _: save_file_dialog.save_file(),
                     disabled=page.web,
                 ),
-                ElevatedButton (
+                ElevatedButton(
                     "打开聊天日志所在文件夹",
                     icon=icons.FOLDER_OPEN,
-                    on_click=lambda _ : get_directory_dialog.get_directory_path (),
+                    on_click=lambda _: get_directory_dialog.get_directory_path(),
                     disabled=page.web,
                 ),
                 settings_btn
             ],
-    alignment = ft.MainAxisAlignment.END,
+            alignment=ft.MainAxisAlignment.END,
         ),
     )
-
-
-    '''
-    添加开局获取apikey窗口
-    '''
-    def save_settings_open(e):
-        open_setting_apikey_dlg.open = False
-        write_APIKEY(apikey_field_open.value)
-        openai.api_key = read_APIKEY ()
-        page.update()
-
-    write_APIKEY()
-    openai.api_key = read_APIKEY()
-    if not openai.api_key:
-        apikey_field_open = ft.TextField ( label="输入你的apikey" )
-        open_setting_apikey_dlg = ft.AlertDialog(
-            open=True,
-            modal=True,
-            title=ft.Text("欢迎使用BillyGPT"),
-            content=ft.Column([apikey_field_open], tight=True),
-            actions=[ft.ElevatedButton(text="开始使用",on_click=save_settings_open)],
-            actions_alignment="end",
-        )
-        page.dialog = open_setting_apikey_dlg
-        openai.api_key = read_APIKEY()
-
-
-    # 设置主页面聊天区域的滚动列表
-    gpt_text = ft.ListView ( expand=True, spacing=10, auto_scroll=True, padding=20 )
-    page.add ( gpt_text )
-
-    # 调用添加聊天行的方法
-    def add_msg(e):
-        gpt_text.controls.append ( chat_row('user',chat_text.value) )
-        chat_text.value = ""
-        page.update()
-        gpt_text.controls.append(chat_row('assistant',chat(chat_text.value)))
-        page.update()
-
-    # 设置布局
-    chat_text = ft.TextField ( hint_text="想和chatGPT说些什么？",filled=True,expand=True,multiline=True )
+    page.add(gpt_text)
+    chat_text = ft.TextField(
+        hint_text="想和chatGPT说些什么？",
+        filled=True,
+        expand=True,
+        multiline=True)
     view = ft.Column(
         controls=[
             ft.Row(
                 controls=[
                     chat_text,
-                    ft.ElevatedButton ( "对话", on_click=add_msg )
+                    ft.ElevatedButton("对话", on_click=add_msg)
                 ]
             ),
         ],
     )
 
-
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.add(view)
 
-
-
-    def chat(msg = None) :
+    def chat(msg=None):
         try:
             print(openai.api_key)
             message = get_combined_data(chat_json_path)
-            completion = openai.ChatCompletion.create (
+            chatGPT_raw_response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=message
             )
-            gpt_msg = completion.choices[0].message['content']
-            d_msg = decode_chr ( gpt_msg )
-            return d_msg
+            chatGPT_response = decode_chr(
+                chatGPT_raw_response.choices[0].message['content'])
+            return chatGPT_response
         except openai.error.AuthenticationError as error:
-            gpt_text.controls.append(ft.Text(f'出现如下报错\n{str(error)}\n请在设置中更新可用的apikey'))
+            gpt_text.controls.append(
+                ft.Text(f'出现如下报错\n{str(error)}\n请在设置中更新可用的apikey'))
             page.update()
         except Exception as error:
-            gpt_text.controls.append ( ft.Text ( f'出现如下报错\n{str ( error )}\n请联系开发者' ) )
+            gpt_text.controls.append(
+                ft.Text(f'出现如下报错\n{str ( error )}\n请联系开发者微信B1lli_official'))
             page.update()
 
-
-
-    # 版本信息
-    ver_text = ft.Text('BillyGPT V3.3.1  By B1lli',size=10)
+    '''
+    版本信息
+    '''
+    ver_text = ft.Text('BillyGPT V3.4.0  哔哩哔哩@B1lli', size=10)
     page.add(ver_text)
 
 
 if __name__ == '__main__':
-    ft.app ( target=ft_interface, assets_dir='assets' )
-
-
-
-
-
-
-
-
+    ft.app(target=ft_interface, assets_dir='assets')
